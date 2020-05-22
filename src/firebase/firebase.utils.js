d101a91b05be907bfc76ba1ada91d2318479fae4
Julian CARLE <carle.julian@gmail.com>
2020-05-22 19:02:53 +0200

    performance optimisation with react lazy, Suspense, memo

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,6 +4,14 @@ import 'firebase/auth'
 
 const config = {
     // YOUR CONFIG HERE
+    apiKey: "AIzaSyBXgjJPrKqYn1bgPLdqA0IgPe1bvlrQCgU",
+    authDomain: "crwn-db-7f5f5.firebaseapp.com",
+    databaseURL: "https://crwn-db-7f5f5.firebaseio.com",
+    projectId: "crwn-db-7f5f5",
+    storageBucket: "crwn-db-7f5f5.appspot.com",
+    messagingSenderId: "931846892727",
+    appId: "1:931846892727:web:ab4a17ee7a30bf70747132",
+    measurementId: "G-D97BD4EF4E"
   };
 firebase.initializeApp(config);
 
